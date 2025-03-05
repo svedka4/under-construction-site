e14b9a2c00c946e655305c318a237faf3ce6638c
Gabija Svedkauskaite <gabija.svedkauskaite@coxautoinc.com>
2025-03-05 21:21:52 +0000

    adds copyright

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 // src/App.tsx
 import useTabTitle from './hooks/useTabTitle';
+import Layout from './components/LayoutComponent/Layout';
 
 function App() {
   useTabTitle('Software Developer & DJ', 'Gabija Svedkauskaite');
 
   return (
-    <div>
-      <p>© 2025 švedka. All rights reserved.</p>
-      <h1 className="text-3x1 font-bold underline">Under Construction</h1>{/* Your content here */}
-    </div>
+    <Layout>
+      <div>
+      </div>
+    </Layout>
   );
 }
 
