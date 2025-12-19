eb5176f7f65036dac980f4bc23e99361dff30db4
Gabija Svedkauskaite <gabija.svedkauskaite@coxautoinc.com>
2025-12-19 15:20:30 +0200

    Swapped background photo

--- src/components/ProfilePhoto/ProfilePhoto.tsx
+++ src/components/ProfilePhoto/ProfilePhoto.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import styles from './ProfilePhoto.module.css';
-import profilePhoto from '../../assets/profile-photo.jpg';
+import profilePhoto from '../../assets/distorted-negative.jpg';
 
 function ProfilePhoto() {
   return (
     <div className={styles.container}>
-      <img src={profilePhoto} alt="Profile" className={styles.image} />
+      <img src={profilePhoto} alt="Distorted photo of a person" className={styles.image} />
     </div>
   );
 };
